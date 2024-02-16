7a551fba10ae69333b2db46049b933c0b549e678
Gustavo W. Teixeira <gustavowt@gmail.com>
2024-02-16 13:21:58 -0300

    Changes on lunnarvim  - remove solargraph  - add obsidian plugin  - configure ruby_lsp

--- config.fish
+++ config.fish
@@ -3,7 +3,9 @@ set -x PATH /usr/local/share/npm/bin $PATH
 set -x PATH ~/go/bin $PATH
 set -x PATH ~/.local/bin $PATH
 set -x PATH /opt/homebrew/bin $PATH
+set -x PATH ~/Library/Python/3.11/bin $PATH
 set -U GEM_HOME ~/.local/share/nvim/lsp_servers/solargraph
+set -Ux OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
 set -Ux PGGSSENCMODE disable
 set -Ux EDITOR lvim
 
--- lunarvim.lua
+++ lunarvim.lua
@@ -10,7 +10,7 @@ an executable
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save.enabled = false
+lvim.format_on_save.enabled = true
 lvim.colorscheme = "kanagawa"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -47,6 +47,8 @@ lvim.builtin.treesitter.ensure_installed = {
   "ruby",
   "java",
   "yaml",
+  "markdown",
+  "markdown_inline"
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -74,28 +76,115 @@ autocmd(
     desc = "Turn off relative line numbering when the buffer is exited.",
   }
 )
-local nvim_lsp = require("lspconfig")
 
-nvim_lsp.solargraph.setup {
-  cmd = { "solargraph", "stdio" },
-  filetypes = { "ruby", "rakefile" },
-  root_dir = nvim_lsp.util.root_pattern("Gemfile", ".git", "."),
-  on_attach = require("lsp-format").on_attach,
-  settings = {
-    solargraph = {
-      autoformat = true,
-      completion = true,
-      diagnostic = true,
-      folding = true,
-      references = true,
-      rename = true,
-      symbols = true
-    }
-  }
-}
+-- Solargraph
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "solargraph" })
+-- remove `ruby_ls` from `skipped_servers` list
+lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+  return server ~= "ruby_ls"
+end, lvim.lsp.automatic_configuration.skipped_servers)
+-- local nvim_lsp = require("lspconfig")
+
+-- nvim_lsp.solargraph.setup {
+--   cmd = { "solargraph", "stdio" },
+--   filetypes = { "ruby", "rakefile" },
+--   root_dir = nvim_lsp.util.root_pattern("Gemfile", ".git", "."),
+--   on_attach = require("lsp-format").on_attach,
+--   settings = {
+--     solargraph = {
+--       autoformat = false,
+--       completion = true,
+--       diagnostic = true,
+--       folding = false,
+--       references = true,
+--       rename = true,
+--       symbols = true
+--     }
+--   }
+-- }
+
+-- Ruby-lsp
+_timers = {}
+local function setup_diagnostics(client, buffer)
+  if require("vim.lsp.diagnostic")._enable then
+    return
+  end
+
+  local diagnostic_handler = function()
+    local params = vim.lsp.util.make_text_document_params(buffer)
+    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+      if err then
+        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+        vim.lsp.log.error(err_msg)
+      end
+      local diagnostic_items = {}
+      if result then
+        diagnostic_items = result.items
+      end
+      vim.lsp.diagnostic.on_publish_diagnostics(
+        nil,
+        vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
+        { client_id = client.id }
+      )
+    end)
+  end
+
+  diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+  vim.api.nvim_buf_attach(buffer, false, {
+    on_lines = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+    end,
+    on_detach = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+    end,
+  })
+end
+
+require("lspconfig").ruby_ls.setup({
+  on_attach = function(client, buffer)
+    setup_diagnostics(client, buffer)
+  end,
+  init_options = {
+    formatter = 'rubocop'
+  },
+})
+
+-- Obsidian
+vim.opt.conceallevel = 1
 
 -- Additional Plugins
 lvim.plugins = {
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "ATON",
+          path = "~/Dropbox/obsidian/vaults/aton",
+        },
+        {
+          name = "personal",
+          path = "~/Dropbox/obsidian/vaults/personal",
+        },
+        {
+          name = "work",
+          path = "~/Dropbox/obsidian/vaults/work",
+        },
+      },
+    },
+  },
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
