5487bdfedfde31bd68ec5d2fd66e68b97b45e143
Gustavo W. Teixeira <gustavowt@gmail.com>
2024-06-23 19:37:21 -0300

    move lsp stuff to lsp config file

--- nvim/lua/custom-bindings.lua
+++ nvim/lua/custom-bindings.lua
@@ -1,13 +1,5 @@
 vim.keymap.set("n", "<S-l>", "<Cmd>bnext<CR>", {})
 vim.keymap.set("n", "<S-h>", "<Cmd>bprevious<CR>", {})
-
--- LSP
-vim.keymap.set("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", {})
-vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", {})
-vim.keymap.set("n", "gr", "<Cmd>lua vim.lsp.buf.references()<cr>", {})
-vim.keymap.set("n", "gI", "<Cmd>lua vim.lsp.buf.implementation()<cr>", {})
-vim.keymap.set("n", "gs", "<Cmd>lua vim.lsp.buf.signature_help()<cr>", {})
-
+--
 -- NvimTree
 vim.keymap.set("n", "F", "<Cmd>:NvimTreeFindFile<CR>", {})
-
