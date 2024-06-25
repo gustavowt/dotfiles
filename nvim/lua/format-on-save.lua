d8c7fefeef46ee9348d930558bdf08614b2d0cc1
Gustavo W. Teixeira <gustavowt@gmail.com>
2024-06-24 23:10:19 -0300

    Skip tssserver from formatting

--- nvim/lua/format-on-save.lua
+++ nvim/lua/format-on-save.lua
@@ -3,6 +3,10 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	group = "format_on_save",
 	pattern = "*",
 	callback = function()
-		vim.lsp.buf.format()
+		vim.lsp.buf.format({
+			filter = function(client)
+				return client.name ~= "tsserver"
+			end,
+		})
 	end,
 })
