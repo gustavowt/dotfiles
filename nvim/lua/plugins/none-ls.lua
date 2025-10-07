5c2395fb61cc91ce2b7de6a4d5f250c9facc67b2
Gustavo Warmling Teixeira <gustavowt@users.noreply.github.com>
2025-10-07 09:56:47 -0300

    Update rubocop format and supress warnings

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -8,7 +8,11 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.diagnostics.erb_lint,
-				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.diagnostics.rubocop.with({
+					-- Redirect stderr to suppress deprecation warnings
+					command = "sh",
+					args = { "-c", "rubocop --format json --force-exclusion --stdin $FILENAME 2>/dev/null" },
+				}),
 				require("none-ls.diagnostics.eslint_d"),
 				-- null_ls.diagnostics.esling_d,
 			},
